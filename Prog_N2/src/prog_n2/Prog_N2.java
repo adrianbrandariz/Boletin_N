8aef0d78f660ba853804b947d39ef77d68a3cd7b
abrandarizdominguez <abrandarizdominguez@victoria9.danielcastelao.org>
2017-10-26 09:04:26 +0200

    Prueba ejercicio N2 COD

---
+++ Prog_N2/src/prog_n2/Prog_N2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog_n2;
+
+/**
+ *
+ * @author abrandarizdominguez
+ */
+public class Prog_N2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
